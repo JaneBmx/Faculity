a6bfde93f58198456709e05f4e635843312c03ea
Jane Sonina <janeridebmx@gmail.com>
2020-04-18 11:43:15 +0300

    Add controller

--- src/main/java/com/vlasova/controller/Controller.java
+++ src/main/java/com/vlasova/controller/Controller.java
@@ -1,5 +1,13 @@
 package com.vlasova.controller;
 
+import com.vlasova.command.Command;
+import com.vlasova.exception.connection.ClosePoolException;
+import com.vlasova.factory.CommandFactory;
+import com.vlasova.params.PageAddress;
+import com.vlasova.pool.ConnectionPool;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,26 +15,38 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/controller")
+@WebServlet(urlPatterns = {"/controller"},
+        name = "controller")
 public class Controller extends HttpServlet {
+    private static Logger LOGGER = LogManager.getLogger(Controller.class);
 
     @Override
-    public void init() throws ServletException {
-
+    public void init() {
+        LOGGER.info("Controller initialization.");
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        processRequest(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        processRequest(req, resp);
     }
 
     @Override
     public void destroy() {
-        super.destroy();
+        try {
+            ConnectionPool.INSTANCE.closePool();
+        } catch (ClosePoolException e) {
+            LOGGER.warn("Fail to close pool.", e);
+        }
+    }
+
+    private void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Command command = CommandFactory.valueOf(req.getParameter("command").toUpperCase()).getCommand();
+        PageAddress pageAddress = command.execute(req, resp);
+        req.getRequestDispatcher(pageAddress.getPath()).forward(req, resp);
     }
 }

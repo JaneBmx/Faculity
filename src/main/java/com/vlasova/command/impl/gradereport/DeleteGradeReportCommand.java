b6a24d948be88deb947b90f60f7b220c802574af
Yana Vlasova <janeridebmx@gmail.com>
2020-06-03 03:07:30 +0300

    Add 'Admin:GradeReports:Delete'

--- src/main/java/com/vlasova/command/impl/gradereport/DeleteGradeReportCommand.java
+++ src/main/java/com/vlasova/command/impl/gradereport/DeleteGradeReportCommand.java
@@ -1,13 +1,32 @@
 package com.vlasova.command.impl.gradereport;
 
 import com.vlasova.command.Answer;
+import com.vlasova.command.web.PageAddress;
+import com.vlasova.exception.service.ServiceException;
+import com.vlasova.service.GradeReportService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class DeleteGradeReportCommand implements GradeReportCommand {
+    private static final Logger LOGGER = LogManager.getLogger(DeleteGradeReportCommand.class);
+    private final GradeReportService gradeReportService = GradeReportService.getInstance();
+
     @Override
     public Answer execute(HttpServletRequest request, HttpServletResponse response) {
-        return null;
+        String gradeId = request.getParameter("grade_report_id");
+        if (gradeId != null) {
+            int gradeID = Integer.parseInt(gradeId);
+            try {
+                gradeReportService.deleteGradeReport(gradeID);
+                LOGGER.info("Delete gradeReport with id:" + gradeID);
+            } catch (ServiceException e) {
+                LOGGER.warn(e);
+                return new Answer(PageAddress.ADMIN_PAGE, Answer.Type.FORWARD);
+            }
+        }
+        return new Answer(PageAddress.ADMIN_PAGE, Answer.Type.REDIRECT);
     }
 }

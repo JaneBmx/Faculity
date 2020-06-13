4c9adc2ef1cd720484ac3e17bacee63f7ee5ee72
Yana Vlasova <janeridebmx@gmail.com>
2020-06-13 20:30:09 +0300

    Fix faculty validator

--- src/main/java/com/vlasova/util/validation/FacultyValidator.java
+++ src/main/java/com/vlasova/util/validation/FacultyValidator.java
@@ -3,27 +3,43 @@ package com.vlasova.util.validation;
 import com.vlasova.entity.faculity.Faculty;
 import com.vlasova.entity.faculity.Subject;
 
-public class FacultyValidator {
-    private static final int MIN = 0;
+import java.util.Set;
 
-    public  boolean isValidFaculty(String name, int free, int paid, Subject... subjects) {
-        return name != null && !name.isEmpty() && free >= MIN && paid >= MIN
-                && free + paid > MIN && isValidSubjects(subjects)
-                && subjects.length!=3;
+public class FacultyValidator {
+    private static final int MIN_PLAN = 0;
+
+    public boolean isValidFaculty(String name, int free, int paid, Set<Subject> subjects) {
+        return isValidName(name) && isValidFreeAcceptPlan(free)
+                && isValidAcceptPlan(paid) && isValidSubjects(subjects);
     }
 
-    public  boolean isValidSubjects(Subject... subjects) {
-        for (Subject s : subjects) {
-            if (s == null) {
-                return false;
+    private boolean isValidSubjects(Set<Subject> subjects) {
+        if (subjects != null && subjects.size() == 3) {
+            for (Subject s : subjects) {
+                if (s == null) {
+                    return false;
+                }
             }
         }
         return true;
     }
 
-    public  boolean isValidFaculty(Faculty faculty) {
-        return isValidFaculty(faculty.getName(), faculty.getFreeAcceptPlan(),
-                faculty.getPaidAcceptPlan(), faculty.getSubjects()
-                        .toArray(new Subject[faculty.getSubjects().size()]));
+    private boolean isValidName(String facultyName) {
+        return facultyName != null && !facultyName.isEmpty();
+    }
+
+    private boolean isValidAcceptPlan(int acceptPlan) {
+        return acceptPlan > MIN_PLAN;
+    }
+
+    private boolean isValidFreeAcceptPlan(int freeAcceptPlan) {
+        return freeAcceptPlan >= MIN_PLAN;
+    }
+
+
+    public boolean isValidFaculty(Faculty faculty) {
+        return faculty != null
+                && isValidFaculty(faculty.getName(), faculty.getFreeAcceptPlan()
+                , faculty.getPaidAcceptPlan(), faculty.getSubjects());
     }
 }
